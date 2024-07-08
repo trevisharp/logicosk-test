9731b4b6ac69f7f6c7e2882cf85740ac73da24d4
Trevisharp <leonardotrevisansilio@gmail.com>
2024-07-08 15:06:43 -0300

    Add Builder function to pratical questions

--- Logicosk/TestBuilder.cs
+++ Logicosk/TestBuilder.cs
@@ -11,14 +11,44 @@ public static class TestBuilder
 {
     const int questionCount = 30;
     const int duration = 60;
+    const int praticalCount = 3;
     public static async Task<Test> CreateTest(string baseFolder)
     {
         var questions = await GetQuestions(
             Path.Combine(baseFolder, "S1"), questionCount
         );
 
-        var test = new Test(baseFolder, duration, questions);
+        var pratical = await GetPraticalTests(
+            Path.Combine(baseFolder, "S2"), praticalCount
+        );
+
+        var test = new Test(baseFolder, duration, questions, pratical);
         return test;
+    }
+
+    public static async Task<List<PraticalTest>> GetPraticalTests(string questionFolder, int size)
+    {
+        var questionFiles = 
+            from file in Directory.GetFiles(questionFolder)
+            where Path.GetExtension(file) == ".json"
+            select file;
+        
+        var options = new JsonSerializerOptions() {
+            PropertyNameCaseInsensitive = true
+        };
+
+        var selectedQuestions = questionFiles
+            .OrderBy(q => Random.Shared.Next())
+            .Take(size)
+            .Select(async file => await File.ReadAllTextAsync(file))
+            .Select(async file => JsonSerializer.Deserialize<PraticalTest>(await file, options))
+            .ToArray();
+        
+        List<PraticalTest> questions = [];
+        foreach (var item in selectedQuestions)
+            questions.Add(await item);
+        
+        return questions;
     }
 
     public static async Task<List<Question>> GetQuestions(string questionFolder, int size)
